c1da8be13240739d666d33b2643141a7a1b5d194
Szymon Galazka <32265060+SzymonGalazka@users.noreply.github.com>
2020-12-06 16:54:45 +0100

    refactor code

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import './Main.css';
-import { getColor, round } from './Helpers';
-const genie = require('@adrianperea/genie.js');
-const { Simulation, Individual, Chromosome } = genie;
+import mcCormickGA from './mcCormickGA';
 
 const Main = () => {
   const [fun, setFun] = useState('mcCormick');
@@ -14,79 +12,10 @@ const Main = () => {
 
   const runAlgorithm = () => {
     if (fun === options[0])
-      computeWithGA(-1.9133, mcCormick, cormickX1, cormickX2);
+      mcCormickGA(-1.9133, mcCormick, cormickX1, cormickX2);
   };
-
   const mcCormick = (x1, x2) =>
     Math.sin(x1 + x2) + (x1 - x2) * (x1 - x2) + 1.0 + 2.5 * x2 - 1.5 * x1;
-
-  const createResultItem = (name) => {
-    let li = document.createElement('li');
-    li.textContent = name;
-    li.style = `background-color: ${getColor()}`;
-    return li;
-  };
-
-  const computeWithGA = (target, testedFun, x1boundary, x2boundary) => {
-    document.querySelector('#results').innerHTML = '';
-    class GlobalMinFinder extends Simulation {
-      calculateFitness(individual, data) {
-        const fitness = data.testedFun(
-          individual.getDna(0)[0],
-          individual.getDna(1)[0]
-        );
-        return fitness / data.target;
-      }
-
-      shouldFinish(top) {
-        return top.fitness === 1;
-      }
-    }
-    const generate = (min, max) => {
-      const x = Math.random() * (max - min) + min;
-      return x;
-    };
-
-    const x1 = new Chromosome(1, () =>
-      generate(parseInt(x1boundary[0]), parseInt(x1boundary[1]))
-    );
-    const x2 = new Chromosome(1, () =>
-      generate(parseInt(x2boundary[0]), parseInt(x2boundary[1]))
-    );
-    const individual = new Individual([x1, x2]);
-    const config = {
-      prototype: individual,
-      data: { target, testedFun },
-      mutationRate: 0.3,
-      popSize: 100,
-      numParents: 2,
-      maxGenerations: 100,
-      elitism: true,
-      selection: genie.ga.Selection.rouletteWheel,
-      crossover: genie.ga.Crossover.multipoint,
-      onCalculateFitness(state) {
-        console.log(state);
-        document.querySelector('#results').appendChild(
-          createResultItem(
-            `Gen ${state.currentGeneration},\n Best Fitness: ${
-              state.top.fitness
-            }, \n
-              [${round(state.top.getDna(0)[0])},${round(
-              state.top.getDna(1)[0]
-            )}]`
-          )
-        );
-        console.log(
-          'Best fitness: ' + state.top.fitness,
-          state.top.getDna(0).join(''),
-          state.top.getDna(1).join('')
-        );
-      },
-    };
-
-    const sim = new GlobalMinFinder(config);
-    sim.start();
-  };
 
   return (
     <div className='main'>
---
+++ src/Components/mcCormickGA.js
@@ -0,0 +1,73 @@
+import { round, getColor } from './Helpers';
+const genie = require('@adrianperea/genie.js');
+const { Simulation, Individual, Chromosome } = genie;
+
+const createResultItem = (name) => {
+  let li = document.createElement('li');
+  li.textContent = name;
+  li.style = `background-color: ${getColor()}`;
+  return li;
+};
+
+const mcCormickGA = (target, testedFun, x1boundary, x2boundary) => {
+  document.querySelector('#results').innerHTML = '';
+  class GlobalMinFinder extends Simulation {
+    calculateFitness(individual, data) {
+      const fitness = data.testedFun(
+        individual.getDna(0)[0],
+        individual.getDna(1)[0]
+      );
+      return fitness / data.target;
+    }
+
+    shouldFinish(top) {
+      return top.fitness === 1;
+    }
+  }
+  const generate = (min, max) => {
+    const x = Math.random() * (max - min) + min;
+    return x;
+  };
+
+  const x1 = new Chromosome(1, () =>
+    generate(parseInt(x1boundary[0]), parseInt(x1boundary[1]))
+  );
+  const x2 = new Chromosome(1, () =>
+    generate(parseInt(x2boundary[0]), parseInt(x2boundary[1]))
+  );
+  const individual = new Individual([x1, x2]);
+  const config = {
+    prototype: individual,
+    data: { target, testedFun },
+    mutationRate: 0.3,
+    popSize: 100,
+    numParents: 2,
+    maxGenerations: 100,
+    elitism: true,
+    selection: genie.ga.Selection.rouletteWheel,
+    crossover: genie.ga.Crossover.multipoint,
+    onCalculateFitness(state) {
+      console.log(state);
+      document.querySelector('#results').appendChild(
+        createResultItem(
+          `Gen ${state.currentGeneration}, Best fitness: ${
+            state.top.fitness
+          }, \n
+              [${round(state.top.getDna(0)[0])},${round(
+            state.top.getDna(1)[0]
+          )}]`
+        )
+      );
+      console.log(
+        'Best fitness: ' + state.top.fitness,
+        state.top.getDna(0).join(''),
+        state.top.getDna(1).join('')
+      );
+    },
+  };
+
+  const sim = new GlobalMinFinder(config);
+  sim.start();
+};
+
+export default mcCormickGA;
